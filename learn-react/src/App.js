e206c78953b6efbe4b129035717e3871555ecff1
skakfk0206 <skakfk0206@gmail.com>
2023-03-27 16:23:57 +0900

    7.조건부 렌더링

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,25 +1,35 @@
-import Avatar from "./Avatar.jsx";
-
-function Card({ children }) {
-  return <div className="card">{children}</div>;
-}
-
-export default function Profile() {
+function Item({ name, isPacked }) {
   return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: "Katsuko Saruhashi",
-          imageId: "YfeOqp2",
-        }}
-      />
-    </Card>
+    <li className="item">
+      {name}
+      {isPacked && "✔"}
+    </li>
+  );
+}
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
   );
 }
 
 /*
-children
-  - 컴포넌트의 여는 태그와 닫는 태그 사이의 내용은 children이라는 약속된 프로퍼티로 전달된다.
-    => 원하는 위치 작성하여 출력할수있다.
+  조번부 렌더링
+  -조건에 따라 보여줄 내용이 달라지거나 혹은 렌더링 여부를 결정할 수 있다.
+
+  1) if문을 통해  조건에 따라 다른 JSX를 return한다.
+     => 간단한 내용보다는 조건에 따라 컴포넌트 자체가 달라지는 것처럼 구조 자체가 바뀔때 사용한다.
+     => 조건의 따라 내용을 렌더링하지 않고자 할 떄는 null을 return하면된다.
+
+  2) 삼항 연산자(? :)
+     =>JSX 안에서는 if문 사용이 불가능 하기떄문에 JSX 안에서 내용을 다르게 할 떄 사용 한다.   
+
+  3) &&(end)
+    => 단순히 앞에 조건에 따라 뒤에 내용이 렌더링 될지말지 정할 떄 사용한다.
 */

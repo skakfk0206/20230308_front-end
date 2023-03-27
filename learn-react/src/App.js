af3e180d698174d29247a5ab5c2571e506fe5763
skakfk0206 <skakfk0206@gmail.com>
2023-03-27 11:25:36 +0900

    2.컴포넌트 export/import

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,20 +1,30 @@
-//컴포넌트 : JS 함수 형태로 작성. 대문자로 시작하는 파스칼케이스.
-
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
-}
-
-export default function Gallery() {
+import Gallery from "./Gallery";
+import { Profile } from "./Gallery"; //named export.
+// src/App.js
+export default function App() {
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <div>
+      <Gallery />
+      <Gallery />
+    </div>
   );
 }
 
+/*
+   컴포넌트 export/import
+
+   -루트 컴퍼넌트는 기본적으로 create-reate-app로 프로젝트를 만들면 ,App.js 무루 컴퍼넌트가된다.
+     => index.js가 가장먼저 실행되고 , 가장 먼저 렌더링 되는 컴퍼넌트가 App컴퍼넌트다.
+     => 언제들 마음대로 변경할수있다.
+
+   - export/import문을 통해서 컴퍼넌트를 별도의 파일로 관리할수있다.
+     =>export default나 named export 둘다 상관없지만 , 핵심이 되는 컴퍼넌트는 export default 권고
+     
+ 
+
+   */
+
+//컴포넌트 : JS 함수 형태로 작성. 대문자로 시작하는 파스칼케이스.
 /*
    컴포넌트
      - UI를JS 함수 형태로 작성하여 조각으로 만들수있다.

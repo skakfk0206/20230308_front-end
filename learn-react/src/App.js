f02f2a2311be2dde4966b187164415c319838d5c
skakfk0206 <skakfk0206@gmail.com>
2023-03-27 12:22:47 +0900

    4.JSX에 자바값 포함시키기.

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,32 +1,38 @@
-export default function TodoList() {
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+}
+
+export function TodoList() {
+  return <h1>To Do List for {formatDate(today)}</h1>;
+}
+
+export default function Avatar() {
+  const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
+  const description = "Gregorio Y. Zara";
+  const title = "React";
+
   return (
-    // This doesn't quite work!
     <>
-      <h1>Hedy Lamarr's Todos</h1>
-      <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        class="photo"
-      />
-      <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
-      </ul>
+      <h1
+        style={{
+          color: "red",
+        }}
+      >
+        Hello {title}!{" "}
+      </h1>
+      <h2>오늘은 {formatDate(today)}</h2>
+      <img className="avatar" src={avatar} alt={description} />;
     </>
   );
 }
 
-/* 
-    JSX (JavaScreipt and XML)
-    - JavaScreipt 마크업 언어를 포함시키기 위한 확장된 문법.
-       => JS 이지만 HTML 구조를 한눈에 알아보기 쉽다.
-    - JSX는 React 개발을 도와주는 하나의 도구이다. 즉, React 별개이다.
-       => create-react-app를 통해 프로젝트를 생성하면 Babel이라는 도구가 포함된다.
-
-    JSX 지켜야될 문법
-
-    1. 컴퍼넌트응 무조건 하나의 루트 요소 를 반환해야한다.
-
-   
+/*
+   JSX 안에 자바 값 (표현식) 포함시키기.
+     - JSX 안에서 {}에 자바표현식을 작성하면 동적인 값을 사용할수 있다.
+       => 변수 ,연산, 함수호출,객체 리터럴등 다양한 자바스크립트 값을 표함할수있다.
+       => 값으로 평가되는 표현식이여야 한다. 즉 if문,for문등은 사용할수없다 .
+     - style 속성을 통해 스타일을 지정할 떄는 객체를 전달해야한다.
+       =>  {{}}형태로  작성한다. 겉의 {} 자자값을 명시하고, 안쪽의 {}를 객체 리터럴이다.
 */

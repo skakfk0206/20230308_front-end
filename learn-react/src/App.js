bec355f306ff279cf42896055e5ac262f861f32b
skakfk0206 <skakfk0206@gmail.com>
2023-03-28 12:20:12 +0900

    11. 객체로 상태 관리하기

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,4 +1,4 @@
-import Counter from "./components/Counter";
+import Counter from "./Components/Pointer";
 
 export default function App() {
   return (
@@ -7,15 +7,3 @@ export default function App() {
     </div>
   );
 }
-
-/* 
-  이벤트 처리
-    - 요소의 on 접두사로 시작하는 이벤트 속성에 함수를 전달한다.
-      => 속성명은 카멜케이스를 사용한다.
-      => 함수 자체를 전달해야 하고, 함수 호출을 하면 안된다.
-      => 인자를 전달하는 등 함수를 호출해야 하는 형태라면, 익명 함수 내에서 함수를 호출하면 된다.
-    - props 객체를 통해서 하위 컴포넌트에 전달할 수 있다.
-    - 하위 요소에서 상위 요소로 같은 이벤트는 전파된다.
-      => e.stopPropagation()을 사용하면 상위 요소로 이벤트 전파를 하지않는다.
-    - form 태그와 같이 태그의 특수한 기능을 실행하지 않고자 할 때는 e.preventDefault()를 사용한다.
-*/

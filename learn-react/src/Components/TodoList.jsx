f921cadf44936ecb7b487d9c8969594e950542f2
skakfk0206 <skakfk0206@gmail.com>
2023-03-29 09:55:18 +0900

    투두리스트 추가, 삭제

--- learn-react/src/Components/TodoList.jsx
+++ learn-react/src/Components/TodoList.jsx
@@ -1,20 +1,51 @@
+import { useState } from "react";
+
 // src/components/TodoList.jsx
-const todos = [
+
+const initialState = [
   { id: 1, text: "React 프로젝트 생성하기", done: true },
   { id: 2, text: "컴포넌트 만들기", done: true },
   { id: 3, text: "상태 관리하기", done: false },
 ];
 
+let nextId = 4;
+
 export default function TodoList() {
+  const [todos, setTodos] = useState(initialState);
+
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, { id: nextId++, text: input, done: false }]);
+  };
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleRemove = (id) => {
+    // filter를 통한 삭제 기능 구현
+    //  => filter는 새로운 배열을 반환한다. 업데이트에 써도 된다.
+    //  => "정말 삭제하시겠습니까?" => 확인 누르면 삭제
+    if (window.confirm("정말 삭제하시겠습니까?"))
+      setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const itemList = todos.map((todo) => (
+    <li key={todo.id} style={{ textDecoration: todo.done && "line-through" }}>
+      {todo.text}
+      <button onClick={() => handleRemove(todo.id)}>삭제</button>
+    </li>
+  ));
+
   return (
     <div>
-      <input type="text" />
-      <button>등록</button>
-      <ul>
-        <li>React 프로젝트 생성하기</li>
-        <li>컴포넌트 만들기</li>
-        <li>상태 관리하기</li>
-      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInput} />
+        <button>등록</button>
+      </form>
+      <ul>{itemList}</ul>
     </div>
   );
 }

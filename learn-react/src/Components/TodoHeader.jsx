f9821b41f2ef8d0861f168a8041b731e18a1b51f
skakfk0206 <skakfk0206@gmail.com>
2023-03-29 15:36:33 +0900

    TodoHeader React.memo 사용

--- learn-react/src/Components/TodoHeader.jsx
+++ learn-react/src/Components/TodoHeader.jsx
@@ -1,13 +1,20 @@
-export default function TodoHeader({ undoneCount }) {
+import React, { useEffect } from "react";
+
+function TodoHeader({ undoneCount }) {
   const dateStr = new Date().toLocaleDateString("ko-KR", {
     dateStyle: "full",
+  });
+
+  useEffect(() => {
+    console.log("Todo Header 렌더링!");
   });
 
   return (
     <div>
       <h2>{dateStr}</h2>
-
       <p>해야할 일 : {undoneCount}</p>
     </div>
   );
 }
+
+export default React.memo(TodoHeader);

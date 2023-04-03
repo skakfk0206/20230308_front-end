ffdeaa4ce1ca056d351746ab3acd5281964a0288
skakfk0206 <skakfk0206@gmail.com>
2023-04-03 16:39:32 +0900

    풀 페이지 애니메이션 완료

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Slider from "./components/Slider";
+
+import Section from "./components/Section";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Slider />);
+root.render(<Section />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

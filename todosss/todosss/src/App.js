6857fb2294c2511258257c9264b5dbd06a0b48d6
skakfk0206 <skakfk0206@gmail.com>
2023-04-04 13:25:56 +0900

    todos 시험
    미완성 시험 예제 변경

---
+++ todosss/todosss/src/App.js
@@ -0,0 +1,93 @@
+import { useState, useRef, useCallback } from "react";
+import ToDoEdit from "./components/ToDoEdit";
+import ToDoInsert from "./components/ToDoInsert";
+import TodoList from "./components/TodoList";
+import TodoTemplate from "./components/ToDoTemplate";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "리액트 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "컴포넌트 스타일링 하기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "투두리스트 만들기",
+      checked: false,
+    },
+  ]);
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [insertToggle, setInsertToggle] = useState(false);
+
+  const nextId = useRef(4);
+  const onInsertToggle = useCallback(() => {
+    if (selectedTodo) {
+      setSelectedTodo((selectedTodo) => null);
+    }
+    setInsertToggle((prev) => !prev);
+  }, [selectedTodo]);
+
+  const onChangeSelectedTodo = (todo) => {
+    setSelectedTodo((selectedTodo) => todo);
+  };
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo)); //concat(): 인자로 주어진 배열이나 값들을 기존 배열에 합쳐서 새 배열 반환
+    nextId.current++; //nextId 1씩 더하기
+  }, []);
+
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
+  const onUpdate = useCallback(
+    (id, text) => {
+      onInsertToggle();
+
+      setTodos((todos) =>
+        todos.map((todo) => (todo.id === id ? { ...todo, text } : todo))
+      );
+    },
+    [onInsertToggle]
+  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  }, []);
+  return (
+    <TodoTemplate>
+      <ToDoInsert onInsert={onInsert} />
+      <TodoList
+        todos={todos}
+        onToggle={onToggle}
+        onRemove={onRemove}
+        onChangeSelectedTodo={onChangeSelectedTodo}
+        onInsertToggle={onInsertToggle}
+      />
+      {insertToggle && (
+        <ToDoEdit
+          onInsert={onInsert}
+          selectedTodo={selectedTodo}
+          onInsertToggle={onInsertToggle}
+          onUpdate={onUpdate}
+          insertToggle={insertToggle}
+        />
+      )}
+    </TodoTemplate>
+  );
+}
+
+export default App;
